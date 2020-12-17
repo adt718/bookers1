69bf27102c8e841a8855dd0d07e3aa13fc39a7d1
ksgses531@gmail.com <ec2-user@ip-172-31-83-217.ec2.internal>
2020-12-17 09:40:06 +0000

    [finish] 12.17

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  # def create
+  # book = Book.new(book_params)
+  # if book.save
+  # redirect_to  book_path(book)
+  # else
+  # @books = Books.all
+  # flash.now[:alert] = "can't be blank error"
+  # render "index"
+  # end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      @books=Book.all
+      render action: :index
+    end
+  end
+
+
+  def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to  books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @books = Book.all
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated.'
+    else
+      render :edit, notice: 'update was failed'
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
